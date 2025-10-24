e900be51509f7e1dbdf087d61afa2e44396e4d36
eloisefang <chenan1@ualberta.ca>
2025-10-24 14:05:28 -0600

    4th commitment

--- app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
+++ app/src/androidTest/java/com/example/androiduitesting/ShowActivityTest.java
@@ -32,36 +32,43 @@ public class ShowActivityTest {
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(androidx.test.espresso.action.ViewActions.typeText("Edmonton"));
         onView(withId(R.id.button_confirm)).perform(click());
-        
+
+        // Click the first city in the list
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
 
+        // Check that ShowActivity is displayed (cityTextView is part of ShowActivity)
         onView(withId(R.id.cityTextView)).check(matches(isDisplayed()));
     }
 
     @Test
     public void testCityNameConsistency() {
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(androidx.test.espresso.action.ViewActions.typeText("Toronto"));
         onView(withId(R.id.button_confirm)).perform(click());
 
+        // Click it
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
 
+        // Verify that the same city name is shown in ShowActivity
         onView(withId(R.id.cityTextView)).check(matches(withText(containsString("Toronto"))));
     }
 
 
     @Test
     public void testBackButtonFunctionality() {
-
+        // Add a city
         onView(withId(R.id.button_add)).perform(click());
         onView(withId(R.id.editText_name)).perform(androidx.test.espresso.action.ViewActions.typeText("Vancouver"));
         onView(withId(R.id.button_confirm)).perform(click());
 
-
+        // Click the city to go to ShowActivity
         onData(anything()).inAdapterView(withId(R.id.city_list)).atPosition(0).perform(click());
 
+        // Tap back button
         onView(withId(R.id.backButton)).perform(click());
 
+        // Check that we're back to MainActivity (list is visible again)
         onView(withId(R.id.city_list)).check(matches(isDisplayed()));
     }
 }
